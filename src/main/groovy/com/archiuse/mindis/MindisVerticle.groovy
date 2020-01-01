f3b9e31e03d5dc24a3fca99e698b3318515e719b
Dmytro Buryak <dmytro.buryak@archiuse.com>
2020-01-01 15:11:50 +0200

    Refactor MindisVerticle

--- src/main/groovy/com/archiuse/mindis/MindisVerticle.groovy
+++ src/main/groovy/com/archiuse/mindis/MindisVerticle.groovy
@@ -1,11 +1,35 @@
 package com.archiuse.mindis
 
 import com.archiuse.mindis.di.Vertx
+import groovy.util.logging.Slf4j
+import io.reactivex.Completable
 import io.vertx.reactivex.core.AbstractVerticle
 
 import javax.inject.Singleton
 
 @Singleton
 @Vertx
+@Slf4j
 abstract class MindisVerticle extends AbstractVerticle {
+    @Override
+    final Completable rxStart() {
+        doStart()
+                .doOnSubscribe { log.info 'starting verticle: {}', this }
+                .doOnComplete { log.info 'verticle started: {}', this }
+    }
+
+    @Override
+    final Completable rxStop() {
+        doStop()
+                .doOnSubscribe { log.info 'stopping verticle: {}', this }
+                .doOnComplete { log.info 'verticle stopped: {}', this }
+    }
+
+    protected Completable doStart() {
+        Completable.complete()
+    }
+
+    protected Completable doStop() {
+        Completable.complete()
+    }
 }
--- src/main/groovy/com/archiuse/mindis/config/ConfigVerticle.groovy
+++ src/main/groovy/com/archiuse/mindis/config/ConfigVerticle.groovy
@@ -2,24 +2,7 @@ package com.archiuse.mindis.config
 
 import com.archiuse.mindis.MindisVerticle
 import groovy.util.logging.Slf4j
-import io.reactivex.Completable
 
 @Slf4j
 class ConfigVerticle extends MindisVerticle {
-
-    @Override
-    Completable rxStart() {
-        Completable
-                .fromAction {}
-                .doOnSubscribe { log.info 'starting config verticle: {}', this }
-                .doOnComplete { log.info 'config verticle started: {}', this }
-    }
-
-    @Override
-    Completable rxStop() {
-        Completable
-                .fromAction {}
-                .doOnSubscribe { log.info 'stopping config verticle: {}', this }
-                .doOnComplete { log.info 'config verticle stopped: {}', this }
-    }
 }
