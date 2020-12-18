90eddedf32532a7c7439f34c752c6501a16a4a85
Dmytro Buryak <dmytro.buryak@gmail.com>
2020-12-18 02:01:23 +0200

    Added more tests for Verticle

--- src/test/groovy/com/dburyak/vertx/core/VerticleSpec.groovy
+++ src/test/groovy/com/dburyak/vertx/core/VerticleSpec.groovy
@@ -62,4 +62,48 @@ class VerticleSpec extends BaseVertxRxJavaSpec {
         and: 'doOnStart hook is NOT called'
         0 * verticle.doOnStart()
     }
+
+    def 'doOnStart failure results in async exception of all the start chain'() {
+        when: 'try to start failing verticle'
+        def res = verticle.rxStart().test().await()
+
+        then: 'no exception is thrown outside the async chain'
+        noExceptionThrown()
+
+        and: 'exception is rather passed down the async chain'
+        res.assertError { err ->
+            err instanceof IllegalArgumentException
+                    && err.message == 'thrown from hook'
+        }
+
+        and: 'startup hook was called and threw an error'
+        1 * verticle.doOnStart() >> Completable.error(new IllegalArgumentException('thrown from hook'))
+
+        and: 'bean context was set up'
+        1 * verticleBeanCtx.registerSingleton(ApplicationContext, verticleBeanCtx, _) >> verticleBeanCtx
+        1 * verticleBeanCtx.registerSingleton(verticle)
+
+        1 * verticleBeanCtx.findBeanRegistration(verticleBeanCtx) >> Optional.of(beanCtxBeanReg)
+        1 * beanCtxBeanReg.identifier >> beanCtxBeanId
+        1 * verticleBeanCtx.refreshBean(beanCtxBeanId)
+
+        1 * verticleBeanCtx.findBeanRegistration(verticle) >> Optional.of(verticleBeanReg)
+        1 * verticleBeanReg.identifier >> verticleBeanId
+        1 * verticleBeanCtx.refreshBean(verticleBeanId)
+    }
+
+    def 'doOnStop failure results in async exception of all the stop chain'() {
+        when:
+        def res = verticle.rxStop().test().await()
+
+        then:
+        noExceptionThrown()
+        res.assertError { err ->
+            err instanceof IllegalArgumentException
+                    && err.message == 'thrown from hook'
+        }
+
+        and:
+        1 * verticle.doOnStop() >> Completable.error(new IllegalArgumentException('thrown from hook'))
+    }
 }
