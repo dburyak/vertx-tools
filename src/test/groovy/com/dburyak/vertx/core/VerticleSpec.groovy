d28bdfba18b6dcc7fea9a55c4276e7fa57233d58
Dmytro Buryak <dmytro.buryak@gmail.com>
2020-12-19 02:56:12 +0200

    Added tests for Verticle readiness checks

--- src/test/groovy/com/dburyak/vertx/core/VerticleSpec.groovy
+++ src/test/groovy/com/dburyak/vertx/core/VerticleSpec.groovy
@@ -236,8 +236,7 @@ class VerticleSpec extends BaseVertxRxJavaSpec {
 
         and:
         1 * verticleVertxCtx.deploymentID() >> 'deploymentId-uuid'
-        1 * verticle.registerHealthProcedures(realHealthChecks) >> { args ->
-            def checks = args[0] as HealthChecks
+        1 * verticle.registerHealthProcedures(realHealthChecks) >> { HealthChecks checks ->
             checks.register('one') { it.complete(Status.OK()) }
             checks.register('two') { it.complete(Status.OK()) }
             checks.register('three') { it.complete(Status.OK()) }
@@ -267,5 +266,112 @@ class VerticleSpec extends BaseVertxRxJavaSpec {
         }
     }
 
+    def 'readiness check calls hook method'() {
+        given: 'no custom ready checks defined'
 
+        when: 'assess readiness of the verticle'
+        def res = verticle
+                .rxStart()
+                .andThen(verticle.ready())
+                .test().await()
+
+        then: 'hook method for subclasses is called'
+        noExceptionThrown()
+        res.assertNoErrors()
+        res.assertValue { it.up }
+        1 * verticle.registerReadyProcedures(readyChecks)
+
+        and:
+        1 * verticleVertxCtx.deploymentID() >> 'deploymentId-uuid'
+        1 * readyChecks.rxCheckStatus() >> Single.just(CheckResult.from(null, Status.OK()))
+
+        and:
+        1 * verticleBeanCtx.registerSingleton(ApplicationContext, verticleBeanCtx, _) >> verticleBeanCtx
+        1 * verticleBeanCtx.registerSingleton(verticle)
+        1 * verticleBeanCtx.findBeanRegistration(verticleBeanCtx) >> Optional.of(beanCtxBeanReg)
+        1 * beanCtxBeanReg.identifier >> beanCtxBeanId
+        1 * verticleBeanCtx.refreshBean(beanCtxBeanId)
+        1 * verticleBeanCtx.findBeanRegistration(verticle) >> Optional.of(verticleBeanReg)
+        1 * verticleBeanReg.identifier >> verticleBeanId
+        1 * verticleBeanCtx.refreshBean(verticleBeanId)
+        1 * verticle.doOnStart() >> Completable.complete()
+    }
+
+    def 'readiness check fails for not deployed verticle'() {
+        given: 'not deployed verticle'
+
+        when: 'assess readiness of not deployed verticle'
+        def res = verticle.ready().test().await()
+
+        then: 'IllegalStateException is thrown'
+        noExceptionThrown()
+        res.assertError { it instanceof IllegalStateException }
+        res.assertError { it.message == 'can not assess readiness of a not deployed verticle' }
+
+        and:
+        1 * verticleVertxCtx.deploymentID() >> null
+        0 * readyChecks.rxCheckStatus()
+    }
+
+    def 'readiness is OK by default'() {
+        given: 'no custom readiness checks defined'
+        verticle.readyChecks = HealthChecks.create(vertx)
+
+        when: 'assess readiness of the verticle'
+        def res = verticle.ready().test().await()
+
+        then: 'readiness of the verticle is OK'
+        noExceptionThrown()
+        res.assertNoErrors()
+        res.assertValue { it.up }
+
+        and:
+        1 * verticleVertxCtx.deploymentID() >> 'deploymentId-uuid'
+    }
+
+    def 'readiness is OK when custom checks are OK'() {
+        given: 'several readiness checks defined that all return OK'
+        def realReadyChecks = HealthChecks.create(vertx)
+        verticle.readyChecks = realReadyChecks
+
+        when: 'access readiness of the verticle'
+        verticle.registerReadyProcedures(realReadyChecks)
+        def res = verticle.ready().test().await()
+
+        then: 'readiness of the verticle is OK'
+        noExceptionThrown()
+        res.assertNoErrors()
+        res.assertValue { it.up }
+
+        and:
+        1 * verticleVertxCtx.deploymentID() >> 'deploymentId-uuid'
+        1 * verticle.registerReadyProcedures(realReadyChecks) >> { HealthChecks checks ->
+            checks.register('one') { it.complete(Status.OK()) }
+            checks.register('two') { it.complete(Status.OK()) }
+            checks.register('three') { it.complete(Status.OK()) }
+        }
+    }
+
+    def 'readiness fails when custom check fails'() {
+        given: 'one of several readiness checks is failing'
+        def realReadyChecks = HealthChecks.create(vertx)
+        verticle.readyChecks = realReadyChecks
+
+        when: 'assess readiness of the verticle'
+        verticle.registerReadyProcedures(realReadyChecks)
+        def res = verticle.ready().test().await()
+
+        then: 'readiness of the verticle is failing'
+        noExceptionThrown()
+        res.assertNoErrors()
+        res.assertValue { !it.up }
+
+        and:
+        1 * verticleVertxCtx.deploymentID() >> 'deploymentId-uuid'
+        1 * verticle.registerReadyProcedures(realReadyChecks) >> { HealthChecks checks ->
+            checks.register('one') { it.complete(Status.OK()) }
+            checks.register('failing') { it.complete(Status.KO()) }
+            checks.register('three') { it.complete(Status.OK()) }
+        }
+    }
 }
