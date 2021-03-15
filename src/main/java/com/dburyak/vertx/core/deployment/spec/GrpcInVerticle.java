6576d2fe74d4f1fea8541e98084d3fb8455f1851
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-03-16 00:43:40 +0200

    Marked grpc verticle deployment specs as not implemented

--- src/main/java/com/dburyak/vertx/core/deployment/spec/GrpcInVerticle.java
+++ src/main/java/com/dburyak/vertx/core/deployment/spec/GrpcInVerticle.java
@@ -4,11 +4,20 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder(toBuilder = true)
 public class GrpcInVerticle extends Verticle {
-    public GrpcInVerticle() {
+
+    protected GrpcInVerticle(GrpcInVerticleBuilder<?, ?> builder) {
+        super(builder);
+        throw new AssertionError("not implemented");
+    }
+
+    @Override
+    public List<String> getAllAddresses() {
         throw new AssertionError("not implemented");
     }
 }
